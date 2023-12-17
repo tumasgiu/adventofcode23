1cecba174df76fb191c7c512f29837773f27ff0d
Thomas Franceschini <tumasgiu@mac.com>
2023-12-17 17:13:22 +0100

    day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	mapset "github.com/deckarep/golang-set/v2"
 	"log"
 	"os"
 	"strconv"
 )
 
 type Symbol struct {
-	x int
-	y int
+	x         int
+	y         int
+	maybeGear bool
 }
 
 type Part struct {
@@ -25,9 +27,22 @@ func (p *Part) Value() int {
 	return int(value)
 }
 
+type Gear struct {
+	Ratio int
+}
+
 type Schematic struct {
 	coords  [][]interface{}
 	symbols []*Symbol
+
+	validParts []*Part
+	gears      []*Gear
+}
+
+func (s *Schematic) AddSymbol(x, y int, maybeGear bool) {
+	sym := &Symbol{x: x, y: y, maybeGear: maybeGear}
+	s.coords[y][x] = sym
+	s.symbols = append(s.symbols, sym)
 }
 
 func (s *Schematic) ParseLine(line string) {
@@ -37,11 +52,11 @@ func (s *Schematic) ParseLine(line string) {
 		switch r {
 		case '.':
 			s.coords[y][x] = nil
+		case '*':
+			s.AddSymbol(x, y, true)
 		default:
 			if _, err := strconv.Atoi(string(r)); err != nil {
-				sym := &Symbol{x: x, y: y}
-				s.coords[y][x] = sym
-				s.symbols = append(s.symbols, sym)
+				s.AddSymbol(x, y, false)
 				continue
 			}
 			if x > 0 {
@@ -56,24 +71,41 @@ func (s *Schematic) ParseLine(line string) {
 	}
 }
 
-func (s *Schematic) ValidParts() []*Part {
-	set := make(map[*Part]struct{})
+func (s *Schematic) process() {
+	s.gears = make([]*Gear, 0)
+	s.validParts = make([]*Part, 0)
+
+	set := mapset.NewSet[*Part]()
 	for _, sym := range s.symbols {
+		symParts := mapset.NewSet[*Part]()
 		for x := sym.x - 1; x <= sym.x+1; x++ {
 			for y := sym.y - 1; y <= sym.y+1; y++ {
 				if p, ok := s.coords[y][x].(*Part); ok {
-					set[p] = struct{}{}
+					set.Add(p)
+					symParts.Add(p)
 				}
 			}
 		}
+		if sym.maybeGear && symParts.Cardinality() == 2 {
+			spp := symParts.ToSlice()
+			s.gears = append(s.gears, &Gear{Ratio: spp[0].Value() * spp[1].Value()})
+		}
 	}
-	pp := make([]*Part, len(set))
-	i := 0
-	for p := range set {
-		pp[i] = p
-		i++
+	s.validParts = append(s.validParts, set.ToSlice()...)
+}
+
+func (s *Schematic) ValidParts() []*Part {
+	if s.validParts == nil {
+		s.process()
 	}
-	return pp
+	return s.validParts
+}
+
+func (s *Schematic) Gears() []*Gear {
+	if s.gears == nil {
+		s.process()
+	}
+	return s.gears
 }
 
 func NewSchematic() Schematic {
@@ -118,6 +150,14 @@ func sumParts(parts []*Part) int {
 	return sum
 }
 
+func sumRatios(gears []*Gear) int {
+	sum := 0
+	for _, p := range gears {
+		sum += p.Ratio
+	}
+	return sum
+}
+
 func main() {
 	schematic, err := load("./test_input.txt")
 	if err != nil {
@@ -130,6 +170,12 @@ func main() {
 		log.Fatalf("Should be 4361 (was %d)", sum)
 	}
 
+	gears := schematic.Gears()
+	sum = sumRatios(gears)
+	if sum != 467835 {
+		log.Fatalf("Should be 467835 (was %d)", sum)
+	}
+
 	schematic, err = load("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -137,4 +183,7 @@ func main() {
 
 	fmt.Println("Part 1 Answer:")
 	fmt.Println(sumParts(schematic.ValidParts()))
+
+	fmt.Println("Part 2 Answer:")
+	fmt.Println(sumRatios(schematic.Gears()))
 }
