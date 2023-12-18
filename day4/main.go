2e54c94d3d62bd319a76f403b26814f6ba231779
Thomas Franceschini <tumasgiu@mac.com>
2023-12-18 11:34:33 +0100

    day4 part 2

--- day4/main.go
+++ day4/main.go
@@ -28,12 +28,38 @@ func parseNumbers(numbers string) []int {
 	return res
 }
 
-func solve(filepath string) int {
+type Node struct {
+	ID   string
+	Next *Node
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+func (l *LinkedList) Insert(id string) {
+	newNode := &Node{
+		ID: id,
+	}
+
+	if l.Head == nil {
+		l.Head = newNode
+	} else {
+		currentNode := l.Head
+		for currentNode.Next != nil {
+			currentNode = currentNode.Next
+		}
+		currentNode.Next = newNode
+	}
+}
+
+func solve(filepath string) (int, int) {
+	stack := make(map[string]*LinkedList)
 	total := 0
 	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println("Error: ", err)
-		return -1
+		return -1, -1
 	}
 
 	defer func(file *os.File) {
@@ -46,7 +72,7 @@ func solve(filepath string) int {
 	re, err := regexp.Compile(`Card\s+(?P<id>\d+):\s+(?P<winning>.+?)\s+\|\s+(?P<drawn>.+)`)
 	if err != nil {
 		fmt.Println("Error compiling regex: ", err)
-		return -1
+		return -1, -1
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -55,6 +81,7 @@ func solve(filepath string) int {
 		match := re.FindStringSubmatch(line)
 		if match != nil {
 			id, winning, drawn := match[1], parseNumbers(match[2]), parseNumbers(match[3])
+			winningCount := 0
 			for _, n := range drawn {
 				if slices.Index(winning, n) != -1 {
 					if points == 0 {
@@ -62,10 +89,17 @@ func solve(filepath string) int {
 					} else {
 						points = points * 2
 					}
+					winningCount++
 				}
 			}
-			fmt.Printf("Card %s: %d points\n", id, points)
+			//fmt.Printf("Card %s: %d points\n", id, points)
 			total += points
+			o := &LinkedList{}
+			for i := 1; i <= winningCount; i++ {
+				j, _ := strconv.Atoi(id)
+				o.Insert(strconv.Itoa(i + j))
+			}
+			stack[id] = o
 		} else {
 			log.Fatal("Unmatched line")
 		}
@@ -75,15 +109,39 @@ func solve(filepath string) int {
 		fmt.Println("Error: ", err)
 	}
 
-	return total
+	cardsCount := 0
+	for _, l := range stack {
+		cardsCount += stackSize(stack, l)
+	}
+
+	return total, cardsCount
+}
+
+func stackSize(stack map[string]*LinkedList, start *LinkedList) int {
+	cardsCount := 1
+	list := start.Head
+
+	for list != nil {
+		id := list.ID
+		o := stack[id]
+		v := stackSize(stack, o)
+		cardsCount += v
+		list = list.Next
+	}
+
+	return cardsCount
 }
 
 func main() {
-	total := solve("./test_input.txt")
+	total, cardsCount := solve("./test_input.txt")
 	if total != 13 {
 		log.Fatal("Should be 13, was", total)
 	}
+	if cardsCount != 30 {
+		log.Fatal("Should be 30, was", cardsCount)
+	}
 
-	total = solve("./input.txt")
+	total, cardsCount = solve("./input.txt")
 	fmt.Println("Part 1 Answer:", total)
+	fmt.Println("Part 2 Answer:", cardsCount)
 }
