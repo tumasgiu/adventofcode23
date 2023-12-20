79cfa40264c3430d3330bbfe1f2da63c9743fefb
Thomas Franceschini <tumasgiu@mac.com>
2023-12-20 12:45:33 +0100

    day6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,7 +7,14 @@ fn main() {
         Race { duration: 72, record: 1089 },
     ];
 
-    println!("Part 1: {}", part1(races))
+    println!("Part 1: {}", part1(races));
+
+    println!("Part 2: {}", part1(vec![
+        Race{
+            duration: 40817772,
+            record: 219101213651089,
+        }
+    ]));
 }
 
 struct Race {
@@ -15,13 +22,13 @@ struct Race {
     record: usize,
 }
 
-fn winning_speeds(race: &Race) -> Vec<usize> {
-    let mut winning_speeds = Vec::new();
+fn winning_speeds_count(race: &Race) -> usize {
+    let mut winning_speeds = 0;
     let mut speed: usize = 0;
     loop {
         let distance_traveled = speed * (race.duration - speed);
         if distance_traveled > race.record {
-            winning_speeds.push(distance_traveled);
+            winning_speeds += 1;
         }
         speed += 1;
         if speed == race.duration {
@@ -32,7 +39,7 @@ fn winning_speeds(race: &Race) -> Vec<usize> {
 }
 
 fn part1(races: Vec<Race>) -> usize {
-    races.iter().map(|r| winning_speeds(r).len()).fold(1, |acc, e| acc * e)
+    races.iter().map(|r| winning_speeds_count(r)).fold(1, |acc, e| acc * e)
 }
 
 #[test]
@@ -44,4 +51,10 @@ fn test_winning_speeds() {
     ];
 
     assert_eq!(288, part1(races));
+
+    let races = vec![
+        Race{ duration: 71530, record: 940200 },
+    ];
+
+    assert_eq!(71503, part1(races));
 }
